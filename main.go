16bc323216270158870952194b9c86caf0122d4c
Semih Buğra <ssrevoss@gmail.com>
2024-08-03 03:07:18 +0300

    Update main.go
    port update

--- main.go
+++ main.go
@@ -13,24 +13,37 @@ import (
 )
 
 func main() {
+    // Load environment variables from .env file
     err := godotenv.Load()
     if err != nil {
         log.Fatalf("Error loading .env file")
     }
 
+    // Initialize the database
     database.InitDB()
 
+    // Create a new HTML template engine
     engine := html.New("./views", ".html")
 
+    // Create a new Fiber app with the template engine
     app := fiber.New(fiber.Config{
         Views: engine,
     })
 
+    // Serve static files from the "./static" directory
     app.Static("/static", "./static")
 
+    // Define routes and their handlers
     app.Get("/", handlers.GetPosts)
     app.Get("/post/:id", handlers.GetPost)
     app.Post("/post", handlers.CreatePost)
 
-    log.Fatal(app.Listen(os.Getenv("PORT")))
+    // Get the port from the environment variable or default to 8000
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8000" // Default port
+    }
+
+    // Start the Fiber app on the specified port
+    log.Fatal(app.Listen(":" + port))
 }
